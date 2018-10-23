8a6fc40eb9f4d5894682060e8d1d9e78f4b9b882
Wout <git@wout.maaskant.info>
2018-10-23 16:22:25 +0200

    docs: Javadoc

--- src/main/kotlin/info/maaskant/wmsnotes/model/eventstore/EventStore.kt
+++ src/main/kotlin/info/maaskant/wmsnotes/model/eventstore/EventStore.kt
@@ -6,22 +6,29 @@ import io.reactivex.Observable
 interface EventStore {
     /**
      * Returns a completing observable that streams all events currently in the repository. Updates are not included.
+     *
+     * @param afterEventId If specified, only events with an id higher than the parameter will be returned.
      */
-    fun getEvents(afterEventId: Int? = null): Observable<Event> // Rename to loadEvents?
+    fun getEvents(afterEventId: Int? = null): Observable<Event>
 
     /**
      * Returns a non-completing observable that streams new events as they are added to the store.
      */
-    // TODO Check if still used
     fun getEventUpdates(): Observable<Event>
 
     /**
      * Returns a completing observable that streams all events applicable to a given note. Updates are not included.
+     *
+     * @param noteId The note id.
+     * @param afterRevision If specified, only events with a revision higher than the parameter will be returned.
      */
     fun getEventsOfNote(noteId: String, afterRevision: Int? = null): Observable<Event>
 
     /**
      * Returns a non-completing observable that streams all events applicable to a given note, including new events as they are added to the store.
+     *
+     * @param noteId The note id.
+     * @param afterRevision If specified, only events with a revision higher than the parameter will be returned.
      */
     fun getEventsOfNoteWithUpdates(noteId: String, afterRevision: Int? = null): Observable<Event> {
         return Observable.concat(
