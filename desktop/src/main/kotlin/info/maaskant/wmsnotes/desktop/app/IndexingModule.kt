1266a717ae5426ffb1d1c8114573a4a8cddb44b0
Wout <git@wout.maaskant.info>
2018-10-27 13:33:44 +0200

    chore: Removed unused imports

--- desktop/src/main/kotlin/info/maaskant/wmsnotes/desktop/app/IndexingModule.kt
+++ desktop/src/main/kotlin/info/maaskant/wmsnotes/desktop/app/IndexingModule.kt
@@ -2,27 +2,9 @@ package info.maaskant.wmsnotes.desktop.app
 
 import dagger.Module
 import dagger.Provides
-import info.maaskant.wmsnotes.client.api.GrpcCommandMapper
-import info.maaskant.wmsnotes.client.api.GrpcEventMapper
 import info.maaskant.wmsnotes.client.indexing.NoteIndex
-import info.maaskant.wmsnotes.client.synchronization.*
-import info.maaskant.wmsnotes.client.synchronization.eventrepository.FileModifiableEventRepository
-import info.maaskant.wmsnotes.client.synchronization.eventrepository.InMemoryModifiableEventRepository
-import info.maaskant.wmsnotes.client.synchronization.eventrepository.ModifiableEventRepository
 import info.maaskant.wmsnotes.desktop.app.Configuration.storeInMemory
-import info.maaskant.wmsnotes.model.CommandProcessor
-import info.maaskant.wmsnotes.model.eventstore.DelayingEventStore
 import info.maaskant.wmsnotes.model.eventstore.EventStore
-import info.maaskant.wmsnotes.model.eventstore.FileEventStore
-import info.maaskant.wmsnotes.model.eventstore.InMemoryEventStore
-import info.maaskant.wmsnotes.model.projection.NoteProjector
-import info.maaskant.wmsnotes.model.projection.cache.*
-import info.maaskant.wmsnotes.server.command.grpc.CommandServiceGrpc
-import info.maaskant.wmsnotes.server.command.grpc.EventServiceGrpc
-import info.maaskant.wmsnotes.utilities.serialization.EventSerializer
-import info.maaskant.wmsnotes.utilities.serialization.KryoEventSerializer
-import io.grpc.ManagedChannel
-import io.grpc.ManagedChannelBuilder
 import io.reactivex.schedulers.Schedulers
 import org.mapdb.DB
 import org.mapdb.DBMaker
