817963d7d1db067481fd0e5bf09151921ba56d3f
Wout <git@wout.maaskant.info>
2018-11-23 19:03:10 +0100

    refactor: Use isDisabled Subject in SynchronizationTask

--- shared/src/main/kotlin/info/maaskant/wmsnotes/client/synchronization/SynchronizationTask.kt
+++ shared/src/main/kotlin/info/maaskant/wmsnotes/client/synchronization/SynchronizationTask.kt
@@ -3,6 +3,7 @@ package info.maaskant.wmsnotes.client.synchronization
 import info.maaskant.wmsnotes.utilities.logger
 import io.reactivex.Observable
 import io.reactivex.disposables.Disposable
+import io.reactivex.rxkotlin.Observables
 import io.reactivex.schedulers.Schedulers
 import io.reactivex.subjects.BehaviorSubject
 import java.util.concurrent.TimeUnit
@@ -16,20 +17,25 @@ class SynchronizationTask @Inject constructor(
 
     private val logger by logger()
 
-    private var paused: BehaviorSubject<Boolean> = BehaviorSubject.createDefault(false)
+    private var isPaused: BehaviorSubject<Boolean> = BehaviorSubject.createDefault(false)
 
     private var timerDisposable: Disposable? = null
 
+    @Synchronized
     fun start() {
         if (timerDisposable == null) {
             logger.debug("Starting synchronization")
-            timerDisposable = Observable
-                    .interval(0, 5, TimeUnit.SECONDS)
-                    .filter { paused.value == false }
-                    .observeOn(Schedulers.io())
-                    .subscribe {
-                        synchronize()
-                    }
+            timerDisposable =
+                    Observables.combineLatest(
+                            Observable.interval(0, 5, TimeUnit.SECONDS),
+                            isPaused
+                    )
+                            .observeOn(Schedulers.io())
+                            .map { (_, isPaused) -> isPaused }
+                            .filter { !it }
+                            .subscribe {
+                                synchronize()
+                            }
         }
     }
 
@@ -38,24 +44,26 @@ class SynchronizationTask @Inject constructor(
         timerDisposable?.dispose()
     }
 
+    @Synchronized
     fun isPaused(): Observable<Boolean> {
-        return paused
+        return isPaused
     }
 
+    @Synchronized
     fun pause() {
-        if (paused.value == false) {
+        if (isPaused.value == false) {
             logger.debug("Pausing synchronization")
-            paused.onNext(true)
+            isPaused.onNext(true)
         }
     }
 
+    @Synchronized
     fun unpause() {
-        if (paused.value == true) {
+        if (isPaused.value == true) {
             logger.debug("Resuming synchronization")
-            paused.onNext(false)
+            isPaused.onNext(false)
         }
     }
-
 
     private fun synchronize() {
         logger.debug("Synchronizing")
@@ -63,5 +71,4 @@ class SynchronizationTask @Inject constructor(
         remoteEventImporter.loadAndStoreRemoteEvents()
         synchronizer.synchronize()
     }
-
 }
