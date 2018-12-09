039663bae19ba40e5726e6e00299d0805c533eb9
scheleaap <git@wout.maaskant.info>
2018-12-09 14:38:06 +0100

    test: Tests for NewSynchronizer

--- shared/src/test/kotlin/info/maaskant/wmsnotes/client/synchronization/NewSynchronizerTest.kt
+++ shared/src/test/kotlin/info/maaskant/wmsnotes/client/synchronization/NewSynchronizerTest.kt
@@ -254,6 +254,72 @@ internal class NewSynchronizerTest {
     }
 
     @Test
+    fun `one note, multiple compensating actions, different events in compensating action`() {
+        // Given
+        val localEvent1 = modelEvent(eventId = 11, noteId = 1, revision = 1)
+        val localEvent2 = modelEvent(eventId = 12, noteId = 1, revision = 2)
+        val remoteEvent1 = modelEvent(eventId = 1, noteId = 1, revision = 11)
+        val remoteEvent2 = modelEvent(eventId = 2, noteId = 1, revision = 12)
+        val localEventForRemoteEvent1 = modelEvent(eventId = 13, noteId = 1, revision = 3)
+        val localEventForRemoteEvent2 = modelEvent(eventId = 14, noteId = 1, revision = 4)
+        val remoteEventForLocalEvent1 = modelEvent(eventId = 3, noteId = 1, revision = 13)
+        val remoteEventForLocalEvent2 = modelEvent(eventId = 4, noteId = 1, revision = 14)
+        every { localEvents.getEvents() }.returns(Observable.just(localEvent1, localEvent2))
+        every { remoteEvents.getEvents() }.returns(Observable.just(remoteEvent1, remoteEvent2))
+        val compensatingAction1 = CompensatingAction(
+                compensatedLocalEvents = listOf(localEvent1),
+                compensatedRemoteEvents = listOf(), // Missing remoteEvent2
+                newLocalEvents = listOf(localEventForRemoteEvent1),
+                newRemoteEvents = listOf(remoteEventForLocalEvent1)
+        )
+        val compensatingAction2 = CompensatingAction(
+                compensatedLocalEvents = listOf(localEvent2),
+                compensatedRemoteEvents = listOf(remoteEvent2),
+                newLocalEvents = listOf(localEventForRemoteEvent2),
+                newRemoteEvents = listOf(remoteEventForLocalEvent2)
+        )
+        every {
+            synchronizationStrategy.resolve(
+                    localEvents = listOf(localEvent1, localEvent2),
+                    remoteEvents = listOf(remoteEvent1, remoteEvent2)
+            )
+        }.returns(Solution(listOf(compensatingAction1, compensatingAction2)))
+        every { compensatingActionExecutor.execute(compensatingAction1) }.returns(ExecutionResult(
+                success = true,
+                newLocalEvents = listOf(EventIdAndRevision(localEventForRemoteEvent1)),
+                newRemoteEvents = listOf(EventIdAndRevision(remoteEventForLocalEvent1))
+        ))
+        every { compensatingActionExecutor.execute(compensatingAction2) }.returns(ExecutionResult(
+                success = true,
+                newLocalEvents = listOf(EventIdAndRevision(localEventForRemoteEvent2)),
+                newRemoteEvents = listOf(EventIdAndRevision(remoteEventForLocalEvent2))
+        ))
+        val s = createSynchronizer()
+        val stateObserver = s.getStateUpdates().test()
+
+        // When
+        s.synchronize()
+
+        // Then
+        verifySequence {
+            localEvents.getEvents()
+            remoteEvents.getEvents()
+            compensatingActionExecutor.execute(compensatingAction1)
+        }
+        verify(exactly = 0) {
+            remoteEvents.removeEvent(remoteEvent1)
+            localEvents.removeEvent(localEvent1)
+            compensatingActionExecutor.execute(compensatingAction2)
+            remoteEvents.removeEvent(remoteEvent2)
+            localEvents.removeEvent(localEvent2)
+        }
+        val finalState = stateObserver.values().last()
+        assertThat(finalState.lastKnownLocalRevisions[localEvent1.noteId]).isEqualTo(localEvent2.revision)
+        assertThat(finalState.lastKnownRemoteRevisions[localEvent1.noteId]).isEqualTo(remoteEvent2.revision)
+        assertThat(finalState.lastSynchronizedLocalRevisions[localEvent1.noteId]).isEqualTo(localEvent2.revision)
+    }
+
+    @Test
     fun `multiple notes, multipe compensating actions`() {
         // Given
         val localEvent1 = modelEvent(eventId = 11, noteId = 1, revision = 1)
@@ -399,7 +465,7 @@ internal class NewSynchronizerTest {
     }
 
     @Test
-    fun `don't process newly created remote events`() {
+    fun `do not synchronize events created during synchronization`() {
         // Given
         val localEvent1 = modelEvent(eventId = 11, noteId = 1, revision = 1)
         val remoteEvent1 = modelEvent(eventId = 1, noteId = 1, revision = 11)
