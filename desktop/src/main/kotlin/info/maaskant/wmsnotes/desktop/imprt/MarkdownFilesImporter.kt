3c19515d036c127e0a7e62fceaba28854aeae5e5
scheleaap <git@wout.maaskant.info>
2018-12-14 21:06:42 +0100

    test: Tests for MarkdownFilesImporter

--- desktop/src/main/kotlin/info/maaskant/wmsnotes/desktop/imprt/MarkdownFilesImporter.kt
+++ desktop/src/main/kotlin/info/maaskant/wmsnotes/desktop/imprt/MarkdownFilesImporter.kt
@@ -1,10 +1,18 @@
 package info.maaskant.wmsnotes.desktop.imprt
 
+import com.google.common.annotations.VisibleForTesting
 import info.maaskant.wmsnotes.model.CommandProcessor
 import java.io.File
+import java.time.Clock
 import javax.inject.Inject
 
-class MarkdownFilesImporter @Inject constructor(private val commandProcessor: CommandProcessor) {
+class MarkdownFilesImporter @VisibleForTesting constructor(
+        private val commandProcessor: CommandProcessor,
+        private val clock: Clock
+) {
+    @Inject
+    constructor(commandProcessor: CommandProcessor) : this(commandProcessor, Clock.systemDefaultZone())
+
     fun import(directory: File) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
--- desktop/src/test/kotlin/info/maaskant/wmsnotes/desktop/imprt/MarkdownFilesImporterTest.kt
+++ desktop/src/test/kotlin/info/maaskant/wmsnotes/desktop/imprt/MarkdownFilesImporterTest.kt
@@ -1,16 +1,18 @@
 package info.maaskant.wmsnotes.desktop.imprt
 
-import info.maaskant.wmsnotes.model.ChangeContentCommand
-import info.maaskant.wmsnotes.model.CommandProcessor
-import info.maaskant.wmsnotes.model.CreateNoteCommand
-import info.maaskant.wmsnotes.model.NoteCreatedEvent
+import info.maaskant.wmsnotes.model.*
 import io.mockk.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.io.File
+import java.time.Clock
+import java.time.ZoneId
+import java.time.ZonedDateTime
 
 internal class MarkdownFilesImporterTest {
     private val noteId = "note"
+    private val clock: Clock = Clock.fixed(ZonedDateTime.of(2001, 2, 3, 4, 5, 6, 7, ZoneId.of("UTC")).toInstant())
+    private val importFolderName: String = "Import of 2001-02-03 04:05:06"
 
     private lateinit var tempDir: File
     private var commandProcessor: CommandProcessor = mockk()
@@ -21,7 +23,7 @@ internal class MarkdownFilesImporterTest {
         clearMocks(
                 commandProcessor
         )
-        every { commandProcessor.blockingProcessCommand(CreateNoteCommand(noteId = null, title = any())) }.returns(NoteCreatedEvent(eventId = 1, noteId = noteId, revision = 1, title = ""))
+        every { commandProcessor.blockingProcessCommand(CreateNoteCommand(noteId = null, path = any(), title = any(), content = any())) }.returns(NoteCreatedEvent(eventId = 1, noteId = noteId, revision = 1, path = Path("el"), title = "", content = ""))
         every { commandProcessor.commands.onNext(any()) }.just(Runs)
     }
 
@@ -35,8 +37,8 @@ internal class MarkdownFilesImporterTest {
         importer.import(tempDir)
 
         // Then
-        verifySequence {
-            commandProcessor.blockingProcessCommand(CreateNoteCommand(noteId = null, title = "Title"))
+        verify {
+            commandProcessor.commands.onNext(CreateNoteCommand(noteId = null, path = Path(importFolderName), title = "Title", content = ""))
         }
     }
 
@@ -51,9 +53,8 @@ internal class MarkdownFilesImporterTest {
         importer.import(tempDir)
 
         // Then
-        verifySequence {
-            commandProcessor.blockingProcessCommand(CreateNoteCommand(noteId = null, title = "Title"))
-            commandProcessor.commands.onNext(ChangeContentCommand(noteId = noteId, lastRevision = 1, content = content))
+        verify {
+            commandProcessor.commands.onNext(CreateNoteCommand(noteId = null, path = Path(importFolderName), title = "Title", content = content))
         }
     }
 
@@ -68,9 +69,8 @@ internal class MarkdownFilesImporterTest {
         importer.import(tempDir)
 
         // Then
-        verifySequence {
-            commandProcessor.blockingProcessCommand(CreateNoteCommand(noteId = null, title = "Title"))
-            commandProcessor.commands.onNext(ChangeContentCommand(noteId = noteId, lastRevision = 1, content = content))
+        verify {
+            commandProcessor.commands.onNext(CreateNoteCommand(noteId = null, path = Path(importFolderName), title = "Title", content = content))
         }
     }
 
@@ -86,11 +86,11 @@ internal class MarkdownFilesImporterTest {
         importer.import(tempDir)
 
         // Then
-        verifySequence {
-            commandProcessor.blockingProcessCommand(CreateNoteCommand(noteId = null, title = "Title"))
-            commandProcessor.commands.onNext(ChangeContentCommand(noteId = noteId, lastRevision = 1, content = importedContent))
+        verify {
+            commandProcessor.commands.onNext(CreateNoteCommand(noteId = null, path = Path(importFolderName), title = "Title", content = importedContent))
         }
     }
+
     @Test
     fun `convert line endings`() {
         // Given
@@ -103,18 +103,40 @@ internal class MarkdownFilesImporterTest {
         importer.import(tempDir)
 
         // Then
-        verifySequence {
-            commandProcessor.blockingProcessCommand(CreateNoteCommand(noteId = null, title = "Title"))
-            commandProcessor.commands.onNext(ChangeContentCommand(noteId = noteId, lastRevision = 1, content = importedContent))
+        verify {
+            commandProcessor.commands.onNext(CreateNoteCommand(noteId = any(), path = any(), title = any(), content = importedContent))
         }
     }
 
-    private fun createMd(filename: String, content: String = "") {
-        tempDir.resolve(filename).writeText(content)
+    @Test
+    fun `folders`() {
+        // Given
+        createMd(path = "el1/el2", filename = "Title.md")
+        val importer = createInstance()
+
+        // When
+        importer.import(tempDir)
+
+        // Then
+        verifySequence {
+            commandProcessor.commands.onNext(CreateFolderCommand(aggregateId = importFolderName, path = Path(importFolderName), title = importFolderName)),
+            commandProcessor.commands.onNext(CreateFolderCommand(aggregateId = "$importFolderName/el1", path = Path(importFolderName, "el1"), title = "el1")),
+            commandProcessor.commands.onNext(CreateFolderCommand(aggregateId = "$importFolderName/el1/el2", path = Path(importFolderName, "el1", "el2"), title = "el2")),
+            commandProcessor.commands.onNext(CreateNoteCommand(noteId = null, path = Path(importFolderName, "el1", "el2"), title = any(), content = any()))
+        }
+    }
+
+    private fun createMd(filename: String, content: String = "", path: String = "") {
+        val dir = if (path == "") {
+            tempDir
+        } else {
+            tempDir.resolve(path)
+        }
+        dir.resolve(filename).writeText(content)
     }
 
 
     private fun createInstance(): MarkdownFilesImporter {
-        return MarkdownFilesImporter(commandProcessor)
+        return MarkdownFilesImporter(commandProcessor, clock)
     }
 }
