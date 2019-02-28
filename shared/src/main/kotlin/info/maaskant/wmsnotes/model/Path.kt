f5f5782f66b842ec33a264063ea2d2fc3ca9c563
Wout <git@wout.maaskant.info>
2019-02-28 10:45:32 +0100

    feat: Path class implementation

--- shared/src/main/kotlin/info/maaskant/wmsnotes/model/Path.kt
+++ shared/src/main/kotlin/info/maaskant/wmsnotes/model/Path.kt
@@ -3,6 +3,15 @@ package info.maaskant.wmsnotes.model
 data class Path(val elements: List<String>) {
     constructor(vararg elements: String) : this(elements.toList())
 
+    init {
+        if (elements.isEmpty()) throw IllegalArgumentException("Empty path")
+        elements
+                .firstOrNull { it.isBlank() || it.contains('/') }
+                ?.let {
+                    throw IllegalArgumentException("Invalid path ($elements)")
+                }
+    }
+
     companion object {
         fun fromString(path: String): Path =
                 Path(path.split('/'))
--- shared/src/test/kotlin/info/maaskant/wmsnotes/model/PathTest.kt
+++ shared/src/test/kotlin/info/maaskant/wmsnotes/model/PathTest.kt
@@ -15,6 +15,40 @@ internal class PathTest {
     }
 
     @Test
+    fun `create with varargs, illegal character`() {
+        assertThrows<IllegalArgumentException> {
+            Path("el/1", "el2", "el3")
+        }
+        assertThrows<IllegalArgumentException> {
+            Path("el1", "el/2", "el3")
+        }
+        assertThrows<IllegalArgumentException> {
+            Path("el1", "el2", "el/3")
+        }
+    }
+
+    @Test
+    fun `create with varargs, empty element`() {
+        assertThrows<IllegalArgumentException> {
+            Path("el1", "", "el3")
+        }
+    }
+
+    @Test
+    fun `create with varargs, blank element`() {
+        assertThrows<IllegalArgumentException> {
+            Path("el1", " ", "el3")
+        }
+    }
+
+    @Test
+    fun `create with varargs, no elements`() {
+        assertThrows<IllegalArgumentException> {
+            Path()
+        }
+    }
+
+    @Test
     fun `create from string`() {
         // When
         val path = Path.fromString("el1/el2/el3")
@@ -24,23 +58,25 @@ internal class PathTest {
     }
 
     @Test
-    fun `empty path`() {
+    fun `create from string, blank`() {
         assertThrows<IllegalArgumentException> {
-            Path()
+            Path.fromString(" ")
         }
     }
 
     @Test
-    fun `empty element`() {
+    fun `create from string, slash problem 1`() {
         assertThrows<IllegalArgumentException> {
-            Path("el1", "", "el3")
+            Path.fromString("/")
         }
     }
 
     @Test
-    fun `blank element`() {
+    fun `create from string, slash problem 2`() {
         assertThrows<IllegalArgumentException> {
-            Path("el1", " ", "el3")
+            Path.fromString("a//b")
         }
     }
+
+
 }
