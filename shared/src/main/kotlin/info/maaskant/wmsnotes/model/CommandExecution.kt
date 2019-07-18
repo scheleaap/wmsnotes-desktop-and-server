5b201988921d05e3cbe0154e745fd9975b814683
scheleaap <git@wout.maaskant.info>
2019-07-18 19:36:30 +0200

    refactor: Make CommandExecution into an object.

--- shared/src/main/kotlin/info/maaskant/wmsnotes/model/CommandExecution.kt
+++ shared/src/main/kotlin/info/maaskant/wmsnotes/model/CommandExecution.kt
@@ -6,32 +6,30 @@ import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.subjects.SingleSubject
 import java.util.concurrent.TimeUnit
 
-class CommandExecution {
-    companion object {
-        private val logger by logger()
+object CommandExecution {
+    private val logger by logger()
 
-        fun executeBlocking(commandBus: CommandBus, commandRequest: CommandRequest<Command>, timeout: Duration?): CommandResult {
-            val subject: SingleSubject<CommandResult> = SingleSubject.create<CommandResult>()
-            commandBus.results
-                    .filter { it.requestId == commandRequest.requestId }
-                    .firstOrError()
-                    .subscribeBy(
-                            onSuccess = subject::onSuccess,
-                            onError = { logger.warn("Error", it) }
-                    )
-            commandBus.requests.onNext(commandRequest)
-            return subject
-                    .compose(withTimeout(timeout))
-                    .blockingGet()
-        }
+    fun executeBlocking(commandBus: CommandBus, commandRequest: CommandRequest<Command>, timeout: Duration?): CommandResult {
+        val subject: SingleSubject<CommandResult> = SingleSubject.create<CommandResult>()
+        commandBus.results
+                .filter { it.requestId == commandRequest.requestId }
+                .firstOrError()
+                .subscribeBy(
+                        onSuccess = subject::onSuccess,
+                        onError = { logger.warn("Error", it) }
+                )
+        commandBus.requests.onNext(commandRequest)
+        return subject
+                .compose(withTimeout(timeout))
+                .blockingGet()
+    }
 
-        private fun withTimeout(duration: Duration?): SingleTransformer<CommandResult, CommandResult> {
-            return SingleTransformer { it2 ->
-                if (duration != null) {
-                    it2.timeout(duration.duration, duration.unit)
-                } else {
-                    it2
-                }
+    private fun withTimeout(duration: Duration?): SingleTransformer<CommandResult, CommandResult> {
+        return SingleTransformer { it2 ->
+            if (duration != null) {
+                it2.timeout(duration.duration, duration.unit)
+            } else {
+                it2
             }
         }
     }
